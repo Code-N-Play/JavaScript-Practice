7653ea670d930a9a9b0a02616629bcbd94db897d
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-06-30 13:44:08 +0530

    15

---
+++ 15_js_string_method/index.js
@@ -0,0 +1,38 @@
+let username = "Neeraj" ;
+
+// console.log(username.charAt(4))
+
+// console.log(username.indexOf("e")) // it finds only first element
+
+// console.log(username.lastIndexOf("e")) // it finds last element
+
+// console.log(username.length)
+
+// username = username.trim() // it i used to trim starting  or ending space 
+// console.log(username)
+
+// console.log(username.toUpperCase())
+
+// console.log(username.toLowerCase())
+
+// console.log(username.repeat(3))
+
+// console.log(username.startsWith(" ")) // checks whether string starts with space or not 
+
+// console.log(username.endsWith(" "))
+
+// console.log(username.includes(" ")) // it checks is username contain space or not 
+
+// let result = username.includes(" ")
+// if (result){
+//     console.log("username does not contain space ");
+// }
+// else{
+//     console.log(username);
+// }
+
+// console.log(username.replaceAll("e" , "A")) // output :- NAAraj 
+
+// console.log(username.padStart(15 , "0")) // it makes string length 15 char long by filling 0 in starting 
+
+console.log(username.padEnd(15 , "0"))
