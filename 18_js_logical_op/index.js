855e644b7802b5241518fca2aa8d951e1a40bafc
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-08 15:15:46 +0530

    18

---
+++ 18_js_logical_op/index.js
@@ -0,0 +1,45 @@
+
+// AND logical op .............&&.............
+
+let age = 18;
+
+if (age < 18 && age >1){
+    console.log("you are not a MEN");
+}
+
+else if (age > 18 && age < 100 ){
+    console.log("you are a MEN ");
+}
+
+else{
+    console.log ("hey you are 18 ..............");
+}
+
+
+
+// OR logical op ..............||...............
+
+let temp = 25;
+
+if (temp <= 0 || temp >30){
+    console.log("temperature is not good");
+}
+
+else {
+    console.log("temperature is good");
+}
+
+
+
+// NOT logical op .............!.................
+
+let sunny = true;
+
+if (!sunny){
+    console.log("it is not sunny");
+}
+else{
+    console.log("it is sunny");
+}
+
+// not logical op ka use boolean value ko flip krne ke liye kiya jata hai 
