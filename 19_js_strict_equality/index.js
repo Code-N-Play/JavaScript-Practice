a7b6bb94425f5cc02ab8218788e338b8926fc308
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-08 15:45:16 +0530

    19

---
+++ 19_js_strict_equality/index.js
@@ -0,0 +1,23 @@
+// = assignment op
+// == comparision op                  only check the value 
+// === strict equality                value or data type dono ko check krta hai 
+// != not equal                       same as comparision use negatively
+// !== strict inequality              same as strict equality but use negatively
+
+let PI = 3.14;
+
+if (PI == "3.14"){
+    console.log("it is same");
+}
+else {
+    console.log("it is not same ");
+}
+
+
+if (PI === "3.14"){
+    console.log("it is same");
+}
+else {
+    console.log("it is not same ");
+}
+
